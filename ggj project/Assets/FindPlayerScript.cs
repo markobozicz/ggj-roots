40cb5c6139581ffa09751c338cf76377fdcd43f6
Starelol <starecustard@gmail.com>
2023-02-04 16:39:12 +0100

    b

--- ggj project/Assets/FindPlayerScript.cs
+++ ggj project/Assets/FindPlayerScript.cs
@@ -8,13 +8,19 @@ public class FindPlayerScript : MonoBehaviour
     public int noticed_range = 10;
     public int chase_range = 20;
     public bool player_chasing = false;
-    public int rabbit_speed = 0;
+    public int rabbit_speed = 2;
+    private Vector3 jump;
+    public float jump_height = 4.5f;
+    private bool isGrounded = false;
 
-    public Transform Player;
+    Rigidbody rb;
+    private Transform Player;
     // Start is called before the first frame update
     void Start()
     {
-
+        Player = GameObject.FindGameObjectWithTag("Player").transform;
+        rb = GetComponent<Rigidbody>();
+        jump = new Vector3(0.0f, jump_height, 0.0f);
     }
 
     // Update is called once per frame
@@ -40,5 +46,22 @@ public class FindPlayerScript : MonoBehaviour
         player_chasing = true;
     }
 
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Ground")
+        {
+            isGrounded = true;
+        }
+        
+        if (player_chasing && collision.gameObject.tag == "Fence" && isGrounded)
+        {
+            rb.AddForce(jump * 2, ForceMode.Impulse);
+            isGrounded = false;
+        }
 
+        if (collision.gameObject.tag == "Player")
+        {
+            rabbit_speed = 0;
+        }
+    }
 }
