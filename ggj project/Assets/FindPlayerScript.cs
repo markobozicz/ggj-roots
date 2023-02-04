c9c97a3350434a03eb04c2d98eeae33f9a457283
Starelol <starecustard@gmail.com>
2023-02-04 15:42:59 +0100

    Find player logic

--- ggj project/Assets/FindPlayerScript.cs
+++ ggj project/Assets/FindPlayerScript.cs
@@ -4,18 +4,40 @@ using UnityEngine;
 
 public class FindPlayerScript : MonoBehaviour
 {
-    public var MoveSpeed = 4;
-    public var MaxDist = 10;
-    public var MinDist = 5;
+    public int MoveSpeed = 4;
+    public int noticed_range = 10;
+    public int chase_range = 20;
+    public bool player_chasing = false;
+
+    public Transform Player;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.LookAt(Player);
+
+        if (Vector3.Distance(transform.position, Player.position) <= noticed_range)
+        {
+            Chase();
+        }
+        else
+        {
+            noticed_range = 10;
+            player_chasing = false;
+        }
     }
+
+    private void Chase()
+    {
+        transform.position += transform.forward * MoveSpeed * Time.deltaTime;
+        noticed_range = 15;
+        player_chasing = true;
+    }
+
+
 }
